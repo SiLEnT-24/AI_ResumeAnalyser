ae4b30158685d595e9c3d28cfa190d4bedb0ba21
SilEnT-24 <ak4689769@gmail.com>
2026-07-17 14:52:01 +0530

    Implement database engine creation with SSL configuration

--- db.py
+++ db.py
@@ -3,4 +3,15 @@ from sqlalchemy.orm import declarative_base, sessionmaker
 
 DATABASE_URL="mysql+mysqldb://41A4e6ajkxvTYaX.root:<PASSWORD>@gateway01.ap-southeast-1.prod.aws.tidbcloud.com:4000/sys?ssl_mode=VERIFY_IDENTITY&ssl_ca=<CA_PATH>"
 
-engine = create_engine()
+engine = create_engine(
+    DATABASE_URL,
+    pool_pre_ping=True,
+    connect_args={
+        "ssl":{
+            "ssl":True
+        }
+    }
+)
+
+SessionLocal= sessionmaker(bind=engine)
+Base = declarative_base()
