c0868aa777644598e1c70bcbb057f54b466212c7
SilEnT-24 <ak4689769@gmail.com>
2026-07-18 18:47:20 +0530

    Update DATABASE_URL to use pymysql and correct database name

--- db.py
+++ db.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 
-DATABASE_URL="mysql+mysqldb://4RjYrYeVdBbPQ9E.root:<PASSWORD>@gateway01.ap-southeast-1.prod.aws.tidbcloud.com:4000/test?ssl_mode=VERIFY_IDENTITY&ssl_ca=<CA_PATH>"
+DATABASE_URL="mysql+pymysql://3xiF46B9f8zPBST.root:JUHwl6JbH38626ac@gateway01.ap-southeast-1.prod.aws.tidbcloud.com:4000/sys?ssl_mode=VERIFY_IDENTITY&ssl_ca=<CA_PATH>"
 
 engine = create_engine(
     DATABASE_URL,
