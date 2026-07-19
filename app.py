f5f3101efadad46cf85951fd6721d4ae18d4b5a4
SilEnT-24 <ak4689769@gmail.com>
2026-07-19 21:22:19 +0530

    Refactor app.py to set up Flask application and improve routing logic for user sessions

--- app.py
+++ app.py
@@ -2,12 +2,25 @@ from flask import Flask, render_templates, request, redirect, session
 from db import Base, engine, SessionLocal 
 
 import models
+import pyPDF2
+import docx
+import json
 
 app = Flask(__name__)
+app.secret_key = "secret123"
 
+Base.metadata.create_all(bind=engine)
+
+#Home
 @app.route("/")
 def home():
-    return "App is running"
+    if "user" in session:
+        return redirect("/dashboard")
+    return redirect("/login")
+
+#--SignUp
+
+@a
 
 if __name__ == "__main__":
     app.run(debug=True)
