e61bb64d2713d4fa7799be363ebc058011299b75
SilEnT-24 <ak4689769@gmail.com>
2026-07-20 12:18:17 +0530

    Remove sensitive database URL from configuration

--- db.py
+++ db.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 
-DATABASE_URL="mysql+mysqldb://iWvLByJ2DGKJTqP.root:hCY1YsUTxIrMDJsv@gateway01.ap-southeast-1.prod.aws.tidbcloud.com:4000/test?ssl_mode=VERIFY_IDENTITY&ssl_ca=<CA_PATH>"
+DATABASE_URL=""
 
 engine = create_engine(
     DATABASE_URL,
